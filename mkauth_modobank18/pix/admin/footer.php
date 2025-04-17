120c5d3325e26ef7f11b8ba32888fa77322f73a6
tallw <46722275+tallw@users.noreply.github.com>
2025-04-17 20:03:21 -0300

    Delete mkauth_modobank18 directory

--- mkauth_modobank18/pix/admin/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-    <strong>&copy; 2024 <a href="#">SEU PROVEDOR</a>.</strong>
-    Todos os direitos reservados.
-</footer>
-
--- mkauth_modobank18/pix/compile.py
+++
@@ -1,4 +0,0 @@
-import py_compile
-
-py_compile.compile('gera.py')
-
